9672642dee968f935b58dc58865bba45eb77bcad
ShivaniSinghDev <shivanisingh8630@gmail.com>
2025-02-07 10:01:05 +0530

    first commit

---
+++ introjava.js
@@ -0,0 +1,264 @@
+// arrow function
+// let sum =()=>{
+//    let a= "hello"
+//    console.log(a)
+// }
+// sum()
+// let a = 10;
+// let b= a;
+// console.log(a)
+// console.log(b)
+// b=1034;
+// console.log(a)
+// console.log(b)
+
+// let a=[1,2,3,4]
+// let b=a;
+// // console.log(b)
+// a[2]=100;
+// console .log(a)
+// console.log(b)
+// a[3]=500;
+// console.log(a)
+// 
+//  exponttion power sign
+// let a=(4**3)
+// console.log(a)
+// console.log(typeof a);
+// remainder operator
+// console.log(30%9)
+// increment
+// let a= 56;
+// console.log(--a)
+// console.log(a++)
+// console.log(++a)
+// let a=4
+// let b=40
+// a=30
+// a=a+b
+// b-=a;
+// console.log(a)
+// a+=10
+// console.log(a)
+// let a= 20 > 30;
+// if( a){
+//       console.log(true)
+// }
+// else{
+//     console.log(false);
+// }
+//  let a="hello shivi"
+//  if(a=="hello"){
+//     console.log("no match")
+//  }
+//  else if( a=="hello shivi"){
+//     console.log("this is right")
+
+//  }
+//  else{
+//     console.log("no data match")
+//  }
+// let student=40;
+// switch(student){
+// case 30:
+//     console.log("student are 30")
+
+// break
+// case 40:
+//     console.log("student are 40");
+//     break
+// case 50:
+//     console.log("student are 50");
+//     break
+//     default:
+//         console.log("no match")
+
+
+// if(false){
+//     let marks=60;
+//     console.log(marks);
+// } 
+// same variable redeclare
+/*let quantity=10;
+if(true){
+    let quantity=12;
+    console.log(quantity)
+}
+console.log(quantity)*/
+// let value=10;+
+
+// if (true){
+// var value=10;
+
+//     value = 20;
+//     alert(value)
+
+// }
+// alert(value);
+// let Name = "shivani";
+// console.log(  Name)
+// console.log(typeof  Name)
+
+// let num=123n;
+// console.log(num);
+// console.log(typeof num)
+// let array=["shivani",123,"kalpna"]
+//     console.log( typeof array);
+// let obj={
+//     Name:"shivani",
+//     Class:12,
+//     address:"pune",
+
+// }
+// console.log(obj.address)
+// // funstion
+// function  sum(){
+//     let a="hello";
+//     console.log(a)
+
+// }
+// sum()
+// exoretion
+// let sum=function(){
+//     let a="hello"
+//     console.log(a)
+// }
+// sum()
+// arrow
+// let sum=()=>{
+//     let a="hello"
+//     console.log(a)
+// } 
+// sum()
+// let a=10;
+// let b=a;
+// console.log(a)
+// console.log(b)
+// b=100
+// console.log(a)
+// console.log(b)
+// let arr1=[1,2,3,4,5,6]
+// arr2=arr1
+// console.log(arr1)
+// console.log(arr2)
+// arr1[2]=100;
+// console.log(arr1)
+// arr2[1]=20;
+// console.log(arr2)
+// operator(+,-,*,%,/,++,--)
+// let a=40;
+// let b=40;
+// if(a>b){
+//     console.log(a+ " is bigest number " +b)
+
+// }
+// else if(a<b){
+//     console.log(a+ " is smallest " + b)
+// }
+// else{
+//     console.log(a+ " is equal to " + b )
+// }
+// let num=45;
+// switch(num){
+// case 30:
+//     console.log("number is 30");
+//     break;
+//     case 40:
+//         console.log("number is 40");
+//         break;
+//         case 50:
+//             console.log("nember is 40")
+//             default:
+//                 console.log("na value match");
+// }
+
+
+// let a= +"23"
+// // console.log( typeof a)
+// let a="shivani"
+// // let details=`my name is `+a;
+// // console.log(details)
+// let detail=my name is $ {a};
+// console.log(detail);
+// let name=prompt("enter your name")
+// let message="my name is " + name
+// console.log(message)
+
+// let name1=prompt("enter your name")
+// let massege1="my name is " +name1
+// console.log(massege1)
+// function intro(){
+//     let name=prompt("enter your name ")
+//     let massege="may name is "+name
+//     console.log(massege)
+// }
+// intro()
+
+// let greet=function(){
+//     let name=prompt("enter your name ")
+//     let message="my name is "+name
+//     console.log(message)  
+//   }
+//   greet()
+// let sum=()=>{
+//     let num1=+prompt("enter first number")
+//     let num2=+prompt("enter second number")
+//     console.log(num1*num2)
+// }
+// sum()
+// let multi=(n1,n2)=>{
+   
+//     console.log(n1,n2)
+//     console.log(n1*2)
+// }
+// let num1=+prompt("enter first number")
+// let num2=+prompt("enter second number")
+// multi(num1,num2)
+// multi(3,2)
+// let message=function(name1="sumit",name2="aman")
+// {
+//     console.log(name1+" is a friend of "+name2)
+
+// }
+// message()
+// let sum=function(){
+//     let message="it is running"
+//     console.log (message)
+//     return message
+// }
+// let value=sum(.)
+// console.log(value);
+
+
+// let  sum = ()=>{
+//     let num1 = 45
+//     let num2 = 30
+//     let valid = num1>num2
+//     return valid
+// }
+
+// sum()
+
+// let value = sum()
+// console.log(value);
+
+
+// if(sum()){
+//     console.log("yes");
+    
+// }
+// else{
+//     console.log("no");
+    
+// }
+//events
+
+
+
+
+
+
+
+
+
+
